09f938b011d4d2ec0dab47c585cfbf4e2b9c4f27
Haloman1123 <136743749+Haloman1123@users.noreply.github.com>
2023-07-27 12:25:26 -0600

    Update lidar.launch.py

--- launch/lidar.launch.py
+++ launch/lidar.launch.py
@@ -1,8 +1,24 @@
-<launch>
-  <node name="lidar_node" pkg="urg_node" type="urg_node" output="screen">
-    <param name="serial_port" value="/dev/ttyUSB0"/>
-    <param name="frame_id" value="lidar_link"/>
-    <param name="angle_compensate" value="true"/>
-    <param name="scan_mode" value="Standard"/>
-  </node>
-</launch>
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+
+        Node(
+            package='urg_node',
+            executable='urg_node_driver',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/ttyACM0',
+                'frame_id': 'laser_frame',
+                'angle_compensate': True,
+                'scan_mode': 'Standard'
+                'serial_baud: 115200'
+                'laser_frame_id: laser'
+ 
+            }]
+        )
+    ])
+
